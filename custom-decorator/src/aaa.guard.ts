6261d13ec24ead10de6cf5a70990c4229c35cf51
H <744769098@qq.com>
2024-03-06 18:08:41 +0800

    装饰器

---
+++ custom-decorator/src/aaa.guard.ts
@@ -0,0 +1,25 @@
+/*
+ * @Date: 2024-03-06 18:01:04
+ * @Description: description
+ */
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AaaGuard implements CanActivate {
+  @Inject(Reflector)
+  private readonly reflector: Reflector;
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    console.log(this.reflector.get('aaa', context.getHandler()));
+    return true;
+  }
+}
--- custom-decorator/src/app.controller.ts
+++ custom-decorator/src/app.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+/*
+ * @Date: 2024-03-06 17:56:48
+ * @Description: description
+ */
+import { Controller, Get, SetMetadata, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AaaGuard } from './aaa.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @SetMetadata('aaa', 'admin')
+  @UseGuards(AaaGuard)
   getHello(): string {
     return this.appService.getHello();
   }
