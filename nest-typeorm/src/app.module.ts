2fede268c3f9b081a8df2195412572e90ff1b717
H <744769098@qq.com>
2024-05-09 13:29:41 +0800

    提交

---
+++ nest-typeorm/src/app.module.ts
@@ -0,0 +1,35 @@
+/*
+ * @Date: 2024-05-09 10:51:00
+ * @Description: description
+ */
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { User } from './user/entities/user.entity';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '118.195.176.186',
+      port: 28002,
+      username: 'root',
+      password: '325600',
+      database: 'practice',
+      synchronize: true,
+      logging: true,
+      entities: [User],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password',
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ nest-typeorm/src/user/entities/user.entity.ts
@@ -0,0 +1,19 @@
+/*
+ * @Date: 2024-05-09 11:04:53
+ * @Description: description
+ */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({
+  name: 'aaa_user',
+})
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    name: 'aaa_name',
+    length: 50,
+  })
+  name: string;
+}
